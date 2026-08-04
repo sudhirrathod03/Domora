175da95a658c011e6559e39755d0465c5e87fc53
Sudhir Rathod <rathodsudhir326@gmail.com>
2026-08-04 23:24:25 +0530

    feat : built booking model

---
+++ backend/models/bookingModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    listing: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Listing",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    checkIn: {
+      type: Date,
+      required: true,
+    },
+    checkOut: {
+      type: Date,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    guests: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "confirmed",
+    },
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+export default Booking;
