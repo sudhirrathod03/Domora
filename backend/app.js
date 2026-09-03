2310d2fee1a1bb30bbc73a0e735f379d1195e8e8
Sudhir Rathod <rathodsudhir326@gmail.com>
2026-09-03 19:06:19 +0530

    REMOVED trailing slash from deployed url

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://domora-black.vercel.app/"],
+    origin: ["http://localhost:5173", "https://domora-black.vercel.app"],
     credentials: true,
   })
 );
