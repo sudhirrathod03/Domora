cfd3a9373804e481852e07c17e488dff644c5f1a
Sudhir Rathod <rathodsudhir326@gmail.com>
2026-09-03 18:54:38 +0530

    created vercel.json

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://domora-cbpe.onrender.com",
   withCredentials: true, // without this cookie will never be stored.
 });
 
